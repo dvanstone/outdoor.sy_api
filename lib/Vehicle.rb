94c421694069dc5a4fb7dd62c48b0fe407a6d91a
Devon Vanstone <devonvanstone@gmail.com>
2022-05-14 17:49:01 -0400

    ODSYAPI-1: test-enabled conversion from line (to be taken from file) to Customer object

--- lib/Vehicle.rb
+++ lib/Vehicle.rb
@@ -1,13 +1,25 @@
 class Vehicle
 
-    t.tring name
-    t.tring type
-    t.float length
+    # def type=(value)
+    #     @type = value
+    # end
+    # def name=(value)
+    #     @name = value
+    # end
+    # def length=(value)
+    #     @length = value
+    # end
+
+    attr_reader :type, :name, :length
 
     def initialize(type, name, length)
         @type = type
         @name = name
         @length = length
+    end
+
+    def ==(other)
+        self.type == other.type && self.name == other.name && self.length == other.length
     end
 
 end

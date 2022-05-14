44ac7c10030fca5d29ea89cc73033930aa556306
Devon Vanstone <devonvanstone@gmail.com>
2022-05-14 18:13:43 -0400

    ODSYAPI-2: receiving basic POST request from Postman

---
+++ app/controllers/api/v1/customer_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::CustomerController < ApplicationController
+
+    # require Customer
+    # require Vehicle
+
+    # POST /api/v1/customer
+    def create
+            # if params["fileName"]
+            #     put params["fileName"]
+            #customer = Customer.createFromString(line)
+            render json: 'WOOOOOOO', status: 200
+    end
+
+end
